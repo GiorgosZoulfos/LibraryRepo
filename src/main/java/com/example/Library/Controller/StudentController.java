8e2e1a2989ba2ed397857f74209b04015b598c0b
Giorgos Zoulfos <giorgoszlf@gmail.com>
2024-10-26 21:46:33 +0300

    Student Controller

---
+++ src/main/java/com/example/Library/Controller/StudentController.java
@@ -0,0 +1,64 @@
+package com.example.Library.Controller;
+
+import com.example.Library.Entities.Student;
+import com.example.Library.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/api/v1")
+public class StudentController {
+
+    private StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    /* ****************************************************** */
+    /* ******************** GET GROUP *********************** */
+    /* ****************************************************** */
+    @GetMapping(path = "/student")
+    public Optional<List<Student>> getAllStudents(){
+        return Optional.ofNullable(studentService.getAllStudents());
+    }
+
+    @GetMapping(path = "/student/{id}")
+    public Optional<Student> getStudentById(@PathVariable("id") int id){
+        return studentService.getStudentById(id);
+    }
+
+    /* ****************************************************** */
+    /* ********************* PUT GROUP ********************** */
+    /* ****************************************************** */
+    @PutMapping(path = "/student")
+    public HttpStatus updateStudentById(@RequestBody Student student){
+        studentService.updateStudent(student);
+        return HttpStatus.CREATED;
+    }
+
+    /* ****************************************************** */
+    /* ******************* DELETE GROUP ********************* */
+    /* ****************************************************** */
+    @DeleteMapping(path = "/student/{id}")
+    public HttpStatus deleteStudentById(@PathVariable("id") int id){
+        studentService.deleteStudent(id);
+        return HttpStatus.OK;
+    }
+
+
+    /* ****************************************************** */
+    /* ******************** POST GROUP ********************** */
+    /* ****************************************************** */
+    @PostMapping(path = "/student")
+    public HttpStatus createNewStudent(@RequestBody Student student){
+        studentService.saveNewStudent(student);
+        return HttpStatus.CREATED;
+    }
+}
+
