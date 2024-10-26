f1d1d38bbb1fa8e99b19cb8a8c68dccd4f9a5b36
Giorgos Zoulfos <giorgoszlf@gmail.com>
2024-10-26 18:34:42 +0300

    Fine JPA Repository

---
+++ src/main/java/com/example/Library/Repository/FineRepository.java
@@ -0,0 +1,9 @@
+package com.example.Library.Repository;
+
+import com.example.Library.Entities.Fine;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FineRepository extends JpaRepository<Fine, Integer> {
+}
