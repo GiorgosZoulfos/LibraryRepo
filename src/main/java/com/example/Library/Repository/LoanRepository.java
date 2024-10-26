01b8924e297f0f0a2c95254a5cc82c48254bd4e1
Giorgos Zoulfos <giorgoszlf@gmail.com>
2024-10-26 18:39:29 +0300

    Change qualifier to @Reposiotry in LoanRepository

--- src/main/java/com/example/Library/Repository/LoanRepository.java
+++ src/main/java/com/example/Library/Repository/LoanRepository.java
@@ -3,7 +3,8 @@ package com.example.Library.Repository;
 import com.example.Library.Entities.Loan;
 import jakarta.persistence.Entity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-@Entity
+@Repository
 public interface LoanRepository extends JpaRepository<Loan, Integer> {
 }
