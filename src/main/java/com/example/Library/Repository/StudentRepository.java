b5edff237ef9ac790bcc2a1d1b22305b1f0190f5
Giorgos Zoulfos <giorgoszlf@gmail.com>
2024-10-26 17:09:58 +0300

    Student JPA Repository

--- src/main/java/com/example/Library/Repository/StudentRepository.java
+++ src/main/java/com/example/Library/Repository/StudentRepository.java
@@ -1,2 +1,9 @@
-package com.example.Library.Repository;public interface StudentRepository {
+package com.example.Library.Repository;
+
+import com.example.Library.Entities.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
 }
